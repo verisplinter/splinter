dd2648a4cfab57f230924823049997107c89dfe8
Jon Howell <howell@vmware.com>
2025-07-24 10:21:53 -0700

    Implement KeyValueFormat using Wrappable

--- Splinter/src/marshalling/KeyValueFormat_v.rs
+++ Splinter/src/marshalling/KeyValueFormat_v.rs
@@ -4,91 +4,57 @@ use vstd::{prelude::*};
 use crate::spec::KeyType_t::*;
 use crate::spec::Messages_t::*;
 use crate::marshalling::Marshalling_v::Deepview;
+use crate::marshalling::IntegerMarshalling_v::IntFormat;
+use crate::marshalling::Wrappable_v::*;
 
 verus! {
+
+pub struct KeyValueFormatWrappable {}
+impl Wrappable for KeyValueFormatWrappable {
+    type AF = IntFormat::<u64>;
+    type BF = IntFormat::<u64>;
+    type DV = (Key,Value);
+    type U = (Key,Value);
+
+    open spec fn value_marshallable(value: (Key, Value)) -> bool
+    {
+        true
+    }
+
+    open spec fn to_pair(value: (Key, Value)) -> (int, int)
+    {
+        (value.0.0 as int, value.1.0 as int)
+    }
+
+    open spec fn from_pair(pair: (int, int)) -> (value: (Key, Value))
+    {
+        (Key(pair.0 as u64), Value(pair.1 as u64))
+    }
+
+    proof fn to_from_bijective()
+    {
+    }
+
+    exec fn exec_to_pair(value: &(Key, Value)) -> (pair: (u64, u64))
+    {
+        (value.0.0, value.1.0)
+    }
+
+    exec fn exec_from_pair(pair: (u64, u64)) -> (km: (Key, Value))
+    {
+        (Key(pair.0 as u64), Value(pair.1 as u64))
+    }
+
+    exec fn new_format_pair() -> (Self::AF, Self::BF)
+    {
+        (IntFormat::new(), IntFormat::new())
+    }
+}
+
+pub type KeyValueFormat = WrappableFormat<KeyValueFormatWrappable>;
 
 impl Deepview<(Key,Value)> for (Key,Value) {
     open spec fn deepv(&self) -> (Key,Value) { (self.0, self.1) }
 }
-
-// pub struct KeyValueFormat {
-//     pub pair_fmt: UniformPairMarshal<IntFormat::<u64>, IntFormat::<u64>>,
-// }
-// 
-// impl KeyValueFormat {
-//     fn new() -> Self
-//     {
-//         KeyValueFormat{
-//             pair_fmt: UniformPairMarshal::new(IntFormat::new(), IntFormat::new()),
-//         }
-//     }
-// }
-// 
-// impl Marshal for KeyValueFormat {
-//     type DV = (Key, Value);
-//     type U = (Key, Value);
-// 
-//     open spec fn valid(&self) -> bool { true }
-// 
-//     open spec fn parsable(&self, data: Seq<u8>) -> bool
-//     {
-//         self.pair_fmt.parsable(data)
-//     }
-// 
-//     open spec fn parse(&self, data: Seq<u8>) -> Self::DV
-//     {
-//         let pair = self.pair_fmt.parse(data);
-//         KeyedMessage{ key: pair.0, message: Message::Define{value: Value(pair.1)} }
-//     }
-// 
-//     exec fn try_parse(&self, slice: &Slice, data: &Vec<u8>) -> (ov: Option<Self::U>)
-//     {
-//         match self.pair_fmt.try_parse(data) {
-//             None => None,
-//             Some(pair) => KeyedMessage{ key: pair.0, message: Message::Define{value: Value(pair.1)}},
-//         }
-//     }
-//     
-//     open spec fn marshallable(&self, value: Self::DV) -> bool
-//     {
-//         self.pair_fmt.marshallable(value)
-//     }
-//         
-//     open spec fn spec_size(&self, value: Self::DV) -> usize
-//     {
-//         self.pair_fmt.spec_size(value)
-//     }
-// 
-//     exec fn exec_size(&self, value: &Self::U) -> (sz: usize)
-//     {
-//         self.pair_fmt.exec_size(value)
-//     }
-// 
-//     exec fn exec_marshall(&self, value: &Self::U, data: &mut Vec<u8>, start: usize) -> (end: usize)
-//     {
-//         let message_data = match value.message {
-//             Message::Define{value: Value(v)} => v,
-//             Message::Update{delta: Delta(_)} => { assume(false); 0 },
-//         };
-//         let pair = (value.key.0, message_data);
-//         self.pair_fmt.exec_marshall(&pair, data, start)
-//     }
-// }
-// 
-// impl UniformSized for KeyValueFormat {
-//     open spec fn uniform_size(&self) -> (sz: usize) {
-//         (u64::uniform_size() + u64::uniform_size()) as usize
-//     }
-// 
-//     proof fn uniform_size_ensures(&self)
-//     ensures 0 < self.uniform_size()
-//     { }
-// 
-//     exec fn exec_uniform_size(&self) -> (sz: usize)
-//     ensures sz == self.uniform_size()
-//     {
-//         u64::exec_uniform_size() + u64::exec_uniform_size()
-//     }
-// }
 
 } //verus!
