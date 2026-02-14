8a62c6e8ac6eab2d1d2f82199f82fd4296743bf2
Jon Howell <jonh@jonh.net>
2026-02-14 14:44:59 -0800

    use verus branch with jialin's remove

--- Splinter/claudes-own-verify-script.sh
+++ Splinter/claudes-own-verify-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-time ~/work/verus-install/verus-x86-linux/verus src/main.rs \
+time ~/work/verus/source/target-verus/release/verus src/main.rs \
     --expand-errors --multiple-errors 5 \
     #--time
